08920a14e64317ccb4649ecbbb289682ce71dcbf
MMialla <meshy112@outlook.com>
2021-07-30 00:37:36 +0300

    added server(s) component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 @NgModule({
   declarations: [
     AppComponent,
 ServerComponent,
+ServersComponent,
   ],
   imports: [
     BrowserModule,
